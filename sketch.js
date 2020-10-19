ff3e8dc7bdf65d3dd64b72c1b919e5f6d2eea954
Pruthvi31 <70841409+Pruthvi31@users.noreply.github.com>
2020-10-19 21:49:27 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,80 +1,99 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
 const Constraint = Matter.Constraint;
-
-var bobOb1, bobOb2, bobOb3, bobOb4, bobOb5;
-var rope1, rope2, rope3, rope4, rope5;
-var roofObject1, roofObject2, roofObject3, roofObject4, roofObject5;
+var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
+var rope1,rope2,rope3, rope4,rope5;
 var world;
 
 
-
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(1600, 700);
 	rectMode(CENTER);
-	
+
+
 	engine = Engine.create();
 	world = engine.world;
-	
-	roofObject=new Roof(width/2,height/2,width/7,20);
-	bobOb1 = new Bob(200,450,40);
-	bobOb2 = new Bob(241,450,40);
-	bobOb3 = new Bob(282,450,40);
-	bobOb4 = new Bob(323,450,40);
-	bobOb5 = new Bob(264,450,40);
-	
+
+	roofObject=new Roof(width/2,height/4,width/7,20);
+
 	bobDiameter=40;
 
+	startBobPositionX=width/2;
+	startBobPositionY=height/4+500;
+	bobObject1=new Bob(startBobPositionX-bobDiameter*2,startBobPositionY,bobDiameter);
+	bobObject2=new Bob(startBobPositionX-bobDiameter,startBobPositionY,bobDiameter);
+	bobObject3=new Bob(startBobPositionX,startBobPositionY,bobDiameter);
+	bobObject4=new Bob(startBobPositionX+bobDiameter,startBobPositionY,bobDiameter);
+	bobObject5=new Bob(startBobPositionX+bobDiameter*2,startBobPositionY,bobDiameter);
+	
+	
+	//Create a Ground
+	
+
 	var render = Render.create({
-		element: document.body,
-		engine: engine,
-		options: {
-		  width: 1200,
-		  height: 700,
-		  wireframes: false
-		}
-	  });
-  
-	  rope1 = new Rope(bobOb1.body, roofObject.body,-bobDiameter*2,0);
-	rope2 = new Rope(bobOb2.body, roofObject.body,-bobDiameter*2,0);
-	rope3 = new Rope(bobOb3.body, roofObject.body,-bobDiameter*2,0);
-	rope4 = new Rope(bobOb4.body, roofObject.body,-bobDiameter*2,0);
-	rope5 = new Rope(bobOb5.body, roofObject.body,-bobDiameter*2,0);
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
+
+
+	rope1=new Rope(bobObject1.body,roofObject.body,-bobDiameter*2, 0)
+
+	rope2=new Rope(bobObject2.body,roofObject.body,-bobDiameter*1, 0)
+	rope3=new Rope(bobObject3.body,roofObject.body,0, 0)
+	rope4=new Rope(bobObject4.body,roofObject.body,bobDiameter*1, 0)
+	rope5=new Rope(bobObject5.body,roofObject.body,bobDiameter*2, 0)
 
 	Engine.run(engine);
+	//Render.run(render);
   
 }
 
 
 function draw() {
-	rectMode(CENTER);
-	background(0);
-	
-	bobOb1.display();
-	bobOb2.display();
-	bobOb3.display();
-	bobOb4.display();
-	bobOb5.display();
-	rope1.display();
-	rope2.display();
-	rope3.display();
-	rope4.display();
-	rope5.display();
-		roofObject.display();
+  rectMode(CENTER);
+  background(230);
+  roofObject.display();
 
+  rope1.display()
+  rope2.display()
+  rope3.display()
+  rope4.display()
+  rope5.display()	
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+
+ 
 }
 
 function keyPressed() {
-	if (keyCode === UP_ARROW) {
+  	if (keyCode === UP_ARROW) {
 
-	  Matter.Body.applyForce(bobOb1.body,bobOb1.body.position,{x:100,y:-185});
-  
-	}
+    	Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-50,y:-45});
+
+  	}
 }
 
 
+function drawLine(constraint)
+{
+	bobBodyPosition=constraint.bodyA.position
+	roofBodyPosition=constraint.bodyB.position
+
+	roofBodyOffset=constraint.pointB;
+	
+	roofBodyX=roofBodyPosition.x+roofBodyOffset.x
+	roofBodyY=roofBodyPosition.y+roofBodyOffset.y
+	line(bobBodyPosition.x, bobBodyPosition.y, roofBodyX,roofBodyY);
+}
 
